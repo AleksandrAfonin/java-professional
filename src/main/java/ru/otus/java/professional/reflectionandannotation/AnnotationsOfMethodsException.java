72560114bfb2771405b0bee03a711598f57f48fc
Aleksandr Afonin <varsup@yandex.ru>
2024-09-19 09:34:26 +0300

    test framework

---
+++ src/main/java/ru/otus/java/professional/reflectionandannotation/AnnotationsOfMethodsException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.professional.reflectionandannotation;
+
+public class AnnotationsOfMethodsException extends Exception {
+  AnnotationsOfMethodsException(String message) {
+    super(message);
+  }
+}
